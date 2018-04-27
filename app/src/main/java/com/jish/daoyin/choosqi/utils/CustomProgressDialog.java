124321990339d0d55684d49ab24ebdc6767cb8d1
jisheng6 <1278661505@QQ>
2018-04-27 13:28:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jish/daoyin/choosqi/utils/CustomProgressDialog.java
@@ -0,0 +1,68 @@
+package com.jish.daoyin.choosqi.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jish.daoyin.choosqi.R;
+
+/**
+ * Created by 20160416CVEG on 2017/8/18.
+ */
+
+public class CustomProgressDialog extends Dialog {
+    private Context context = null;
+    private static CustomProgressDialog customProgressDialog = null;
+
+    public CustomProgressDialog(Context context){
+        super(context);
+        this.context = context;
+    }
+
+    public CustomProgressDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    public static CustomProgressDialog createDialog(Context context){
+        customProgressDialog = new CustomProgressDialog(context, R.style.CustomProgressDialog);
+
+        customProgressDialog.setContentView(R.layout.customprogressdialog);
+
+        customProgressDialog.getWindow().getAttributes().gravity = Gravity.CENTER;
+
+        customProgressDialog.setCancelable(false);//这样就不能响应返回键的事件了
+        return customProgressDialog;
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus){
+
+        if (customProgressDialog == null){
+            return;
+        }
+        ImageView imageView = (ImageView) customProgressDialog.findViewById(R.id.loadingImageView);
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getBackground();
+        animationDrawable.start();
+    }
+
+    /**
+     *   setTitile 标题
+     */
+    public CustomProgressDialog setTitile(String strTitle){
+        return customProgressDialog;
+    }
+
+    /**
+     *  setMessage 提示内容
+     */
+    public CustomProgressDialog setMessage(String strMessage){
+        TextView tvMsg = (TextView)customProgressDialog.findViewById(R.id.id_tv_loadingmsg);
+
+        if (tvMsg != null){
+            tvMsg.setText(strMessage);
+        }
+        return customProgressDialog;
+    }
+}
